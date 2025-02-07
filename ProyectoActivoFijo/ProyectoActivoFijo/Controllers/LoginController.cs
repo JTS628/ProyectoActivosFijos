aa5ca45c4ddd3d4933c003b3f35946a982e04e92
Jose Ureña Aguilar <urea.jose@hotmail.com>
2025-02-06 21:30:10 -0600

    Login

---
+++ ProyectoActivoFijo/ProyectoActivoFijo/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoActivoFijo.Models;
+
+namespace ProyectoActivoFijo.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Authenticate(User user)
+        {
+            // Aquí puedes agregar lógica de autenticación con una base de datos
+            if (user.Username == "admin" && user.Password == "1234")
+            {
+                return RedirectToAction("Dashboard");
+            }
+            ViewBag.Error = "Usuario o contraseña incorrectos";
+            return View("Index");
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View("Dashboard");
+        }
+    }
+}
