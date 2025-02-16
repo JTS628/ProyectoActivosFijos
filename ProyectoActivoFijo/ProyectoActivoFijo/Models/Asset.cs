c829860e0494b07566081442d837aca702605728
JTS628 <jj628@hotmail.com>
2025-02-16 17:30:47 -0600

    view para agregar activos
    se agrego una nueva vista
    View addAsset
    Model Asset
    Controler Asset

---
+++ ProyectoActivoFijo/ProyectoActivoFijo/Models/Asset.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoActivoFijo.Models
+{
+    public class Activos
+    {
+        public int ID { get; set; } 
+        public string nombre { get; set; }  
+        public string descripcion { get; set; }
+        public string ubicacion { get; set; }
+        public string estado { get; set; }
+        public string fechaAdquisicion { get; set; }
+    }
+}
