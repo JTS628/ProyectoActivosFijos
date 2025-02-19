1d3efae2f7e4f582efd8f7a9f30c7c6e32fcb80f
Florecitaa <160565962+Florecitaa@users.noreply.github.com>
2025-02-18 20:13:33 -0600

    Creacion de modelo historial de cambio y controlador reportes

---
+++ ProyectoActivoFijo/ProyectoActivoFijo/Models/HistorialCambio.cs
@@ -0,0 +1,16 @@
+﻿namespace ProyectoActivoFijo.Models
+{
+    public class HistorialCambio
+    {
+        public int Id { get; set; }
+        
+        public DateTime Fecha { get; set; }
+
+        public required string UsuarioEmail { get; set; } /*Esto es para saber quien realizo el cambio*/
+        
+        public required string Accion { get; set; }
+
+        public required string Detalles { get; set; }
+
+    }
+}
