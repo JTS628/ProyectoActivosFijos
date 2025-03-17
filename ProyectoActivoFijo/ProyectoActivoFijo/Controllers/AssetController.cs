5ac9969b96bc7d069a9a971147a98d00c4cba400
Florecitaa <160565962+Florecitaa@users.noreply.github.com>
2025-03-17 14:06:12 -0600

    Cambios a controller

--- ProyectoActivoFijo/ProyectoActivoFijo/Controllers/AssetController.cs
+++ ProyectoActivoFijo/ProyectoActivoFijo/Controllers/AssetController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Google.Cloud.Firestore;
+using Microsoft.AspNetCore.Mvc;
  
 using ProyectoActivoFijo.Models;
 
@@ -20,25 +21,69 @@ namespace ProyectoActivoFijo.Controllers
             return View();
         }
 
-        // GET: AssetController/Edit/5
-        public async Task<ActionResult> Edit(int id)
+        //Aqui deje el anterior en caso de que no sea el ideal y se pueda restablecer 
+        //// GET: AssetController/Edit/5
+        //public async Task<ActionResult> Edit(int id)
+        //{
+        //    var filtros = new Dictionary<string, (string, object)>
+        //    { 
+        //        { "id", ("EQUAL", id) },
+        //    };
+
+        //    Asset asset = new Asset();
+        //    List<Asset> listAssets = await fbc.GetDataAsync<Asset>("activos", filtros);
+
+        //    if (listAssets.Count != 1)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(listAssets.FirstOrDefault());
+
+        //}
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(string id, Asset updatedAsset) 
         {
-            var filtros = new Dictionary<string, (string, object)>
-            { 
-                { "id", ("EQUAL", id) },
-            };
-
-            Asset asset = new Asset();
-            List<Asset> listAssets = await fbc.GetDataAsync<Asset>("activos", filtros);
-
-            if (listAssets.Count != 1)
+            try
             {
-                return NotFound();
-            }
+                FirestoreDb db = FirestoreDb.Create("dotnet-activos-fijos");
+                DocumentReference docRef = db.Collection("activos").Document(id);
+                DocumentSnapshot snapshot = await docRef.GetSnapshotAsync();
 
-            return View(listAssets.FirstOrDefault());
-            
+                if (!snapshot.Exists)
+                    return NotFound();
+
+                await docRef.UpdateAsync(new Dictionary<string, object>
+        {
+            {"Nombre", updatedAsset.Nombre},
+            {"Estado", updatedAsset.Estado},
+            {"Ubicacion", updatedAsset.Ubicacion},
+            {"Descripcion", updatedAsset.Descripcion},
+            {"Fecha", updatedAsset.FechaAdquisicion.ToUniversalTime()}
+        });
+
+                
+                HistorialCambio cambio = new HistorialCambio
+                {
+                    Fecha = DateTime.Now,
+                    UsuarioEmail = "usuariotest@domain.com", 
+                    Accion = "Edición de Activo",
+                    Detalles = $"Se editó el activo {updatedAsset.ID}: {updatedAsset.Nombre}"
+                };
+
+                AssetHelper assetHelper = new AssetHelper();
+                await assetHelper.SaveHistorialCambio(cambio);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(updatedAsset);
+            }
         }
+
 
         // GET: AssetController/Details/5
         public ActionResult Details(int id)
@@ -54,11 +99,34 @@ namespace ProyectoActivoFijo.Controllers
             return View();
         }
 
-        public ActionResult CreateAsset(string txtId, string txtNombre, string txtEstado, string txtUbicacion, string txtDescripcion, DateTime txtFechadquisicion )
-        {
-            AssetHelper assetHelper = new AssetHelper(); 
+        //Anterior, se puede restablecer en caso de que no sea el ideal
+        //public ActionResult CreateAsset(string txtId, string txtNombre, string txtEstado, string txtUbicacion, string txtDescripcion, DateTime txtFechadquisicion )
+        //{
+        //    AssetHelper assetHelper = new AssetHelper(); 
 
-            bool result = assetHelper.saveAsset(new Asset
+        //    bool result = assetHelper.saveAsset(new Asset
+        //    {
+        //        ID = txtId,
+        //        Nombre = txtNombre,
+        //        Estado = txtEstado,
+        //        Ubicacion = txtUbicacion,
+        //        Descripcion = txtDescripcion,
+        //        FechaAdquisicion = txtFechadquisicion,
+
+
+        //    }).Result;
+
+        //    return RedirectToAction("Index");
+
+        //}
+
+        [HttpPost]
+        public async Task<ActionResult> CreateAsset( string txtId, string txtNombre, string txtEstado, string txtUbicacion, string txtDescripcion, DateTime txtFechadquisicion)
+        {
+            AssetHelper assetHelper = new AssetHelper();
+
+            
+            bool result = await assetHelper.saveAsset(new Asset
             {
                 ID = txtId,
                 Nombre = txtNombre,
@@ -66,13 +134,25 @@ namespace ProyectoActivoFijo.Controllers
                 Ubicacion = txtUbicacion,
                 Descripcion = txtDescripcion,
                 FechaAdquisicion = txtFechadquisicion,
+            });
 
+            if (result)
+            {
+               
+                HistorialCambio cambio = new HistorialCambio
+                {
+                    Fecha = DateTime.Now,
+                    UsuarioEmail = "usuariotest@domain.com", //aqui supongo que pone el correo de admin? en caso que, se cambia
+                    Accion = "Creación de Activo",
+                    Detalles = $"Se creó el activo {txtId}: {txtNombre}"
+                };
 
-            }).Result;
+                await assetHelper.SaveHistorialCambio(cambio);
+            }
 
             return RedirectToAction("Index");
-
         }
+
 
 
         // POST: AssetController/Create
